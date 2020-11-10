82d51895b6d38b3dcf9b87ae556d257192cb8d0a
Dreptu <james.martin@asdnh.org>
2020-11-09 19:31:54 -0500

    Added code for changing between players

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -20,6 +20,46 @@ void TicTacToe::drawBoard{} {
 }
 
 
-void TicTacToe::makeMove(int move) {
-    
+void TicTacToe::makeMove(char player) {
+
+    int move;
+    cout << "\n" << "Enter number you want to make your move in: ";
+    cin >> move
+
+    if( move = 1) {
+		board[0][0] = player;
+	}
+	else if(move = 2) {
+		board[0][1] = player;
+	}
+	else if(move = 3) {
+		board[0][2] = player;
+	}
+	else if(move = 4) {
+		board[1][0] = player;
+	}
+	else if(move = 5) {
+		board[1][1] = player;
+	}
+	else if(move = 6) {
+		board[1][2] = player;
+	}
+	else if(move = 7) {
+		board[2][0] = player;
+	}
+	else if(move = 8) {
+		board[2][1] = player;
+	}
+	else if(move = 9) {
+		board[2][2] = player;
+	}
+}
+
+void TicTacToe::togglePlayer(char player) {
+    if (player == "X")
+        player = "O";
+    else if(player == "O")
+        player = "X";
+}
+
 }
